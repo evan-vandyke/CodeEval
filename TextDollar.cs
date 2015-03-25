08beda85fc90fe011886cb3e4dd69f1c44ebca7e
Evan VanDyke <vandyke09@gmail.com>
2015-03-24 23:18:32 -0400

    Adding challenge description

--- TextDollar.cs
+++ TextDollar.cs
@@ -4,7 +4,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+/*CHALLENGE DESCRIPTION:
 
+Credits: This challenge has been authored by Terence Rudkin 
+
+You are given a positive integer number. This represents the sales made that day 
+in your department store. The payables department however, needs this printed out 
+in english. NOTE: The correct spelling of 40 is Forty. (NOT Fourty)
+
+INPUT SAMPLE:
+
+Your program should accept as its first argument a path to a filename.The input 
+file contains several lines. Each line is one test case. Each line contains a 
+positive integer
+
+OUTPUT SAMPLE:
+
+For each set of input produce a single line of output which is the english textual 
+representation of that integer. The output should be unspaced and in Camelcase.
+Always assume plural quantities. You can also assume that the numbers are < 1000000000 (1 billion).
+In case of ambiguities e.g. 2200 could be TwoThousandTwoHundredDollars or 
+TwentyTwoHundredDollars, always choose the representation with the larger 
+base i.e. TwoThousandTwoHundredDollars.
+*/
 namespace TextDollar
 {
 	class Program
