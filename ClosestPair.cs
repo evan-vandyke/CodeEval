b5e9b1e1f24721f81dfe2b2d832229ade4725431
Evan VanDyke <vandyke09@gmail.com>
2015-03-24 23:11:15 -0400

    Adding challenge description

--- ClosestPair.cs
+++ ClosestPair.cs
@@ -4,6 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+/*CHALLENGE DESCRIPTION:
+
+Credits: Programming Challenges by Steven S. Skiena and Miguel A. Revilla 
+
+You will be given the x/y co-ordinates of several locations. 
+You will be laying out 1 cable between two of these locations. 
+In order to minimise the cost, your task is to find the shortest distance
+between a pair of locations, so that pair can be chosen for the cable installation.
+
+INPUT SAMPLE:
+
+Your program should accept as its first argument a path to a filename. 
+The input file contains several sets of input. Each set of input starts with 
+an integer N (0<=N<=10000), which denotes the number of points in this set. 
+The next N line contains the coordinates of N two-dimensional points. 
+The first of the two numbers denotes the X-coordinate and the latter denotes 
+the Y-coordinate. The input is terminated by a set whose N=0. 
+This set should not be processed. The value of the coordinates will be less 
+than 40000 and non-negative. */
 
 namespace ClosestPair
 {
